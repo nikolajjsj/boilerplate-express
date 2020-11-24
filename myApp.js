351bb718f38e514797778352267f493e3e018590
Nikolaj Johannes Skole Jensen <10490273+nikolajjsj@users.noreply.github.com>
2020-11-24 10:31:50 +0100

    Added /json path to serve json data object

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// add a /json path that serves json data
+app.get('/json', function (req, res) {
+  res.json({ message: 'Hello json' });
 });
 
 module.exports = app;
